90991bede8e8932a1f4e08fa924899b1fb7959ff
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-08-01 11:23:44 -0300

    style: criação do estilo global

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle `
+    * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+    }
+
+    :root {
+        --primary-color: #116193;
+        --secundary-color: #00ede7;
+        --auxiliary-color: #ebebeb;
+        --text-color: #5e5e5e;
+    }
+    
+    body {
+        background: #fff;
+        -webkit-font-smoothing: antialiased;
+        color: var(--text-color);
+        font-family: 'Abel', 'Lato', sans-serif;
+    }
+`; 
